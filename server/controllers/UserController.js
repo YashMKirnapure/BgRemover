91b46f24ea3614b2cc30d2263e521c49141506b2
YashKirnapure77 <yashkirnapure777@gmail.com>
2024-11-03 15:22:15 +0530

    Doing backend work

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -1,9 +1,45 @@
-
+import { Webhook } from "svix"
 
 // API Controller function to manage clerk user with database
 // http://localhost:4000/api/user/webhooks
 
 const clerkWebhooks = async (req,res) =>
 {
-    
+    try {
+        //Create a Svix instance with clerk webhook secret.
+        const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET)
+        await whook.verify(JSON.stringify(req.body),{
+            "svix-id":req.headers["svix-id"],
+            "svix-timestamp":req.headers["svix-timestamp"],
+            "svix-signature":req.headers["svix-signature"]
+        })
+        const {data,type} = req.body
+
+        switch (type) {
+            case "user.created":{
+                const userData = {
+                    clerkId: data.id,
+                    email: data.email_addresses[0].email_address,
+                    firstName: data.first_name,
+                    lastName: data.last_name,
+                    photo: data.image_url
+                }
+                await userModel.create(userData);
+                res.json({});
+                break;
+            }
+            case "user.updated":{
+                break;
+            }     
+            case "user.deleted":{
+                break;
+            }     
+            default:
+                break;
+        }
+
+    } catch (error) {
+        console.log(error.message)
+        res.json({success:false,message:error.message});
+    }
 }
