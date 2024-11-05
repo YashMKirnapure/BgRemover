e088b9eb8f2022a29e04abacd62c875666435f49
YashKirnapure77 <yashkirnapure777@gmail.com>
2024-11-05 10:31:28 +0530

    API Integration

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,43 +1,86 @@
 import { createContext, useState } from "react";
-import {useAuth} from '@clerk/clerk-react';
-import axios from 'axios';
-import {toast} from 'react-toastify';
-export const AppContext = createContext()
+import { useAuth, useClerk, useUser } from "@clerk/clerk-react";
+import axios from "axios";
+import { toast } from "react-toastify";
+import {useNavigate} from "react-router-dom";
 
-const AppContextProvider = (props) =>
-{
-    const {credit,setCredit} = useState(false);
+export const AppContext = createContext();
 
-    const backendUrl = import.meta.env.VITE_BACKEND_URL
+const AppContextProvider = (props) => {
+  const [credit, setCredit] = useState(false);
+  const [image, setImage] = useState(false);
+  const [resultImage,setResultImage] = useState(false);
 
-    const {getToken} = useAuth
+  const backendUrl = import.meta.env.VITE_BACKEND_URL;
+  const navigate = useNavigate()
 
-    const loadCreditsData = async () =>
-    {
-        try {
-            const token = await getToken();
-            const {data} = await axios.get(backendUrl+'/api/user/credits',{headers:{token}})
-            if(data.success)
-            {
-                setCredit(data.credits);
-                console.log(data.credits);           
-            }
-        } catch (error) {
-            console.log(error);
-            toast.error(error.message);
+  const { getToken } = useAuth();
+  const { isSignedIn } = useUser();
+  const { openSignIn } = useClerk();
+
+  const loadCreditsData = async () => {
+    try {
+      const token = await getToken();
+      const { data } = await axios.get(backendUrl + "/api/user/credits", {
+        headers: { token },
+      });
+      if (data.success) {
+        setCredit(data.credits);
+        console.log(data.credits);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
+    }
+  };
+
+  const removeBg = async (image) => {
+    try {
+      if (!isSignedIn) {
+        return openSignIn();
+      }
+      setImage(image);
+      setResultImage(false);
+
+      navigate('/result')
+
+      const token = await getToken()
+
+      const formData = new FormData()
+      image && formData.append('image',image)
+
+      const {data} = await axios.post(backendUrl+'/api/image/remove-bg',formData,{headers:{token}})
+
+      if(data.success){
+        setResultImage(data.resultImage)
+        data.creditBalance && setCredit(data.creditBalance)
+    }
+    else{
+        toast.error(data.message)
+        data.creditBalance && setCredit(data.creditBalance)
+        if(data.creditBalance === 0){
+            navigate('/buy')
         }
-    }
+      }
 
-    const value = {
-        credit,setCredit,
-        loadCreditsData,
-        backendUrl
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
     }
-    return (
-        <AppContext.Provider value={value}>
-            {props.children}
-        </AppContext.Provider>
-    )
-}
+  };
 
-export default AppContextProvider
+  const value = {
+    credit,
+    setCredit,
+    loadCreditsData,
+    backendUrl,
+    image,
+    setImage,
+    removeBg,
+  };
+  return (
+    <AppContext.Provider value={value}>{props.children}</AppContext.Provider>
+  );
+};
+
+export default AppContextProvider;
